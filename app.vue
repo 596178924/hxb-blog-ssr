b6212dddc55a595d9f4f6a83882485de8e530cfd
zengzhaochen <596178924@qq.com>
2024-04-09 20:14:09 +0800

    添加防御性代码

--- app.vue
+++ app.vue
@@ -4,6 +4,11 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 防御性代码，防止被嵌入到其他网页中
+if (window.top !== window.self) {
+  window.top.location.href = window.self.location.href
+}
+</script>
 
 <style lang="scss" scoped></style>
